7d47d0fc07d120470d4f7059dacd0c7bb23c091f
anassarrhioui <anassarrhioui@gmail.com>
2022-12-24 23:05:12 +0100

    .git

--- contravention/contravention-command-service/src/main/java/me/arrhioui/contraventioncommandservice/service/OverSpeedEventHandler.java
+++ contravention/contravention-command-service/src/main/java/me/arrhioui/contraventioncommandservice/service/OverSpeedEventHandler.java
@@ -21,7 +21,6 @@ import java.util.stream.Collectors;
 public class OverSpeedEventHandler {
 
     private final CommandGateway commandGateway;
-    private EventStore eventStore;
 
 
     @EventHandler
